cd5504c66e27ea56d8fee39b5dae8fb28b16068e
blairmuraoka <31453468+blairmuraoka@users.noreply.github.com>
2018-04-04 13:35:22 -1000

    updates on consolidation

--- HawaiiAlertSystem.java
+++ HawaiiAlertSystem.java
@@ -30,162 +30,239 @@ import javafx.scene.control.TitledPane;
  */
 public class HawaiiAlertSystem extends Application {
 
-  @Override
-  public void start(Stage primaryStage) {
+	@Override
+	public void start(Stage primaryStage) {
 
-    primaryStage.setTitle("Hawaii Alert System");
+		primaryStage.setTitle("Hawaii Alert System");
 
-    primaryStage.show();
+		primaryStage.show();
 
-    BorderPane border = new BorderPane();
-    HBox initTop = new HBox();
-    initTop.setAlignment(Pos.CENTER);
-    initTop.setPadding(new Insets(20, 10, 20, 10));
+		BorderPane border = new BorderPane();
+		HBox initTop = new HBox();
+		initTop.setAlignment(Pos.CENTER);
+		initTop.setPadding(new Insets(20, 10, 20, 10));
 
-    initTop.setStyle("-fx-background-color: #38cdff");
+		initTop.setStyle("-fx-background-color: #38cdff");
 
-    Text scenetitle = new Text("Hawaii Alert System");
-    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 80));
+		Text scenetitle = new Text("Hawaii Alert System");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 80));
 
-    initTop.getChildren().add(scenetitle);
-    border.setTop(initTop);
+		initTop.getChildren().add(scenetitle);
+		border.setTop(initTop);
 
-    VBox rightBox = new VBox();
-    rightBox.setStyle("-fx-background-color: #ededed");
-    rightBox.setPrefWidth(100);
-    border.setRight(rightBox);
+		VBox rightBox = new VBox();
+		rightBox.setStyle("-fx-background-color: #ededed");
+		rightBox.setPrefWidth(100);
+		border.setRight(rightBox);
 
-    VBox leftBox = new VBox();
-    leftBox.setStyle("-fx-background-color: #ededed");
-    leftBox.setPrefWidth(100);
-    border.setLeft(leftBox);
+		VBox leftBox = new VBox();
+		leftBox.setStyle("-fx-background-color: #ededed");
+		leftBox.setPrefWidth(100);
+		border.setLeft(leftBox);
 
-    GridPane grid = new GridPane();
-    grid.setAlignment(Pos.TOP_CENTER);
-    grid.setHgap(10);
-    grid.setVgap(25);
-    grid.setPadding(new Insets(80, 25, 25, 25));
-    grid.setStyle("-fx-background-color: #ededed");
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.TOP_CENTER);
+		grid.setHgap(10);
+		grid.setVgap(25);
+		grid.setPadding(new Insets(80, 25, 25, 25));
+		grid.setStyle("-fx-background-color: #ededed");
 
-    Text instruction = new Text("Please select a choice:");
-    instruction.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-    grid.add(instruction, 0, 0, 2, 1);
+		Text instruction = new Text("Please select a choice:");
+		instruction.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+		grid.add(instruction, 0, 0, 2, 1);
 
-    Button testButton = new Button("TEST");
-    testButton.setPrefSize(400, 80);
-    testButton.setStyle("-fx-font: 40 Tahoma; -fx-base: #00870d");
-    testButton.setOnAction(new EventHandler<ActionEvent>() {
+		Button testButton = new Button("TEST");
+		testButton.setPrefSize(400, 80);
+		testButton.setStyle("-fx-font: 40 Tahoma; -fx-base: #00870d");
+		testButton.setOnAction(new EventHandler<ActionEvent>() {
 
-      @Override
-      public void handle(ActionEvent event) {
-        initTop.setStyle("-fx-background-color: #00870d");
-        scenetitle.setText("TEST");
-        scenetitle.setFill(Color.WHITE);
-        rightBox.setStyle("-fx-background-color: #00870d");
-        rightBox.setPrefWidth(100);
-        leftBox.setStyle("-fx-background-color: #00870d");
-        leftBox.setPrefWidth(100);
+			@Override
+			public void handle(ActionEvent event) {
+				initTop.setStyle("-fx-background-color: #00870d");
+				scenetitle.setText("TEST");
+				scenetitle.setFill(Color.WHITE);
+				rightBox.setStyle("-fx-background-color: #00870d");
+				rightBox.setPrefWidth(100);
+				leftBox.setStyle("-fx-background-color: #00870d");
+				leftBox.setPrefWidth(100);
 
-      }
-    });
-    grid.add(testButton, 0, 2);
+			}
+		});
+		grid.add(testButton, 0, 2);
 
-    Button alertButton = new Button("ALERT");
-    alertButton.setPrefSize(400, 80);
-    alertButton.setStyle("-fx-font: 40 Tahoma; -fx-base: #d10000");
-    alertButton.setOnAction(new EventHandler<ActionEvent>() {
+		Button alertButton = new Button("ALERT");
+		alertButton.setPrefSize(400, 80);
+		alertButton.setStyle("-fx-font: 40 Tahoma; -fx-base: #d10000");
+		alertButton.setOnAction(new EventHandler<ActionEvent>() {
 
-      @Override
-      public void handle(ActionEvent event) {
-        initTop.setStyle("-fx-background-color: #d10000");
-        scenetitle.setText("ALERT");
-        scenetitle.setFill(Color.WHITE);
-        rightBox.setStyle("-fx-background-color: #d10000");
-        leftBox.setStyle("-fx-background-color: #d10000");
-        border.setCenter(choices());
-      }
-    });
-    grid.add(alertButton, 1, 2);
+			@Override
+			public void handle(ActionEvent event) {
+				initTop.setStyle("-fx-background-color: #d10000");
+				scenetitle.setText("ALERT");
+				scenetitle.setFill(Color.WHITE);
+				rightBox.setStyle("-fx-background-color: #d10000");
+				leftBox.setStyle("-fx-background-color: #d10000");
+				border.setCenter(choices());
+			}
+		});
+		grid.add(alertButton, 1, 2);
 
-    border.setCenter(grid);
-
-
-
-    Scene scene = new Scene(border, 800, 600);
-    primaryStage.setScene(scene);
+		border.setCenter(grid);
 
 
-  }
 
-    Accordion choices() { // might change to a scene return value
-
-      VBox content1 = new VBox();
-      BorderPane border = new BorderPane();
-      HBox initTop = new HBox();
-      initTop.setAlignment(Pos.CENTER);
-      initTop.setPadding(new Insets(20, 10, 20, 10));
-      Text scenetitle = new Text("Please select alert type:");
-      initTop.getChildren().add(scenetitle);
-      border.setTop(initTop);
-      
-      
-
-      TitledPane t1 = new TitledPane();
-      t1.setText("Choices: ");
-      GridPane grid = new GridPane();
-      grid.setPadding(new Insets(5, 5, 5, 5));
-      ToggleGroup tg = new ToggleGroup();
-      ToggleButton tgb[] = new ToggleButton[8];
-      tgb[0] = new ToggleButton("B1");
-      tgb[1] = new ToggleButton("B2");
-      tgb[2] = new ToggleButton("B3");
-      tgb[3] = new ToggleButton("B4");
-      tgb[4] = new ToggleButton("B5");
-      tgb[5] = new ToggleButton("B6");
-      tgb[6] = new ToggleButton("B7");
-      tgb[7] = new ToggleButton("B8");
-
-      for (ToggleButton i : tgb) {
-        i.setToggleGroup(tg);
-        i.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent e) {
-            t1.setText("Choice: " + i.getText());
-          }
-        });
-      }
-
-      grid.add(tgb[0], 1, 0);
-      grid.add(tgb[1], 2, 0);
-      grid.add(tgb[2], 3, 0);
-      grid.add(tgb[3], 4, 0);
-      grid.add(tgb[4], 1, 1);
-      grid.add(tgb[5], 2, 1);
-      grid.add(tgb[6], 3, 1);
-      grid.add(tgb[7], 4, 1);
-      grid.setAlignment(Pos.CENTER);
-      border.setCenter(grid);
-      // border.setCenter(grid);
-      content1.getChildren().add(border);
-      t1.setContent(content1);
-      TitledPane t2 = new TitledPane("T2", new Button("A2"));
-      TitledPane t3 = new TitledPane("T3", new Button("A3"));
-
-      Accordion accordion = new Accordion();
-      accordion.getPanes().addAll(t1, t2, t3);
-      accordion.setExpandedPane(t1);
-      
-      return accordion;
-    }
+		Scene scene = new Scene(border, 800, 600);
+		primaryStage.setScene(scene);
 
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-      launch(args);
-    }
+	}
 
-    
+	Accordion choices() { // might change to a scene return value
+		Accordion accordion = new Accordion();
+		VBox content1 = new VBox();
+		BorderPane border = new BorderPane();
+		HBox initTop = new HBox();
+		// TitledPane t1 = new TitledPane();
+		TitledPane t1, t2, t3;
+		t1 = choicesPane(accordion, t2);
+		t2 = new TitledPane("T2", new Button("A2"));
+		t3 = new TitledPane("T3", new Button("A3"));
+		accordion.getPanes().addAll(t1, t2, t3);
+		accordion.setExpandedPane(t1);
 
-  }
+		// initTop.setAlignment(Pos.CENTER);
+		// initTop.setPadding(new Insets(20, 10, 20, 10));
+		// Text scenetitle = new Text("Please select alert type:");
+		// initTop.getChildren().add(scenetitle);
+		// border.setTop(initTop);	
+
+		// t1.setText("Choices: ");
+		// GridPane grid = new GridPane();
+		// grid.setPadding(new Insets(5, 5, 5, 5));
+		// ToggleGroup tg = new ToggleGroup();
+		// ToggleButton tgb[] = new ToggleButton[8];
+		// tgb[0] = new ToggleButton("B1");
+		// tgb[1] = new ToggleButton("B2");
+		// tgb[2] = new ToggleButton("B3");
+		// tgb[3] = new ToggleButton("B4");
+		// tgb[4] = new ToggleButton("B5");
+		// tgb[5] = new ToggleButton("B6");
+		// tgb[6] = new ToggleButton("B7");
+		// tgb[7] = new ToggleButton("B8");
+
+		// for (ToggleButton i : tgb) {
+		// 	i.setToggleGroup(tg);
+		// 	i.setOnAction(new EventHandler<ActionEvent>() {
+		// 		@Override
+		// 		public void handle(ActionEvent e) {
+		// 			t1.setText("Choice: " + i.getText());
+		// 		}
+		// 	});
+		// }
+
+		// grid.add(tgb[0], 1, 0);
+		// grid.add(tgb[1], 2, 0);
+		// grid.add(tgb[2], 3, 0);
+		// grid.add(tgb[3], 4, 0);
+		// grid.add(tgb[4], 1, 1);
+		// grid.add(tgb[5], 2, 1);
+		// grid.add(tgb[6], 3, 1);
+		// grid.add(tgb[7], 4, 1);
+		// grid.setAlignment(Pos.CENTER);
+		// border.setCenter(grid);
+
+		// HBox nextButtonBox1 = new HBox();
+		// nextButtonBox1.setPadding(new Insets(100, 0, 0, 0));
+		// Button nextButton = new Button("Next");
+		// nextButtonBox1.getChildren().add(nextButton);
+		// nextButton.setOnAction(new EventHandler<ActionEvent>() {
+		// 	@Override
+		// 	public void handle(ActionEvent e) {
+		// 		accordion.setExpandedPane(t2);
+		// 	}
+		// });
+		// nextButtonBox1.setAlignment(Pos.BOTTOM_CENTER);
+		// border.setBottom(nextButtonBox1);      
+		// content1.getChildren().add(border);
+		// t1.setContent(content1);
+
+
+
+		return accordion;
+	}
+
+	TitledPane choicesPane(Accordion a, TitledPane next) {
+		VBox content1 = new VBox();
+		BorderPane border = new BorderPane();
+		HBox initTop = new HBox();
+		TitledPane t1 = new TitledPane();
+
+		initTop.setAlignment(Pos.CENTER);
+		initTop.setPadding(new Insets(20, 10, 20, 10));
+		Text scenetitle = new Text("Please select alert type:");
+		initTop.getChildren().add(scenetitle);
+		border.setTop(initTop);	
+
+		t1.setText("Choices: ");
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(5, 5, 5, 5));
+		ToggleGroup tg = new ToggleGroup();
+		ToggleButton tgb[] = new ToggleButton[8];
+		tgb[0] = new ToggleButton("B1");
+		tgb[1] = new ToggleButton("B2");
+		tgb[2] = new ToggleButton("B3");
+		tgb[3] = new ToggleButton("B4");
+		tgb[4] = new ToggleButton("B5");
+		tgb[5] = new ToggleButton("B6");
+		tgb[6] = new ToggleButton("B7");
+		tgb[7] = new ToggleButton("B8");
+
+		for (ToggleButton i : tgb) {
+			i.setToggleGroup(tg);
+			i.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent e) {
+					t1.setText("Choice: " + i.getText());
+				}
+			});
+		}
+
+		grid.add(tgb[0], 1, 0);
+		grid.add(tgb[1], 2, 0);
+		grid.add(tgb[2], 3, 0);
+		grid.add(tgb[3], 4, 0);
+		grid.add(tgb[4], 1, 1);
+		grid.add(tgb[5], 2, 1);
+		grid.add(tgb[6], 3, 1);
+		grid.add(tgb[7], 4, 1);
+		grid.setAlignment(Pos.CENTER);
+		border.setCenter(grid);
+
+		HBox nextButtonBox1 = new HBox();
+		nextButtonBox1.setPadding(new Insets(100, 0, 0, 0));
+		Button nextButton = new Button("Next");
+		nextButtonBox1.getChildren().add(nextButton);
+		nextButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				a.setExpandedPane(next);
+			}
+		});
+		nextButtonBox1.setAlignment(Pos.BOTTOM_CENTER);
+		border.setBottom(nextButtonBox1);      
+		content1.getChildren().add(border);
+		t1.setContent(content1);
+
+		return t1;
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+
+}
