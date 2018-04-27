c6c85b2242029493b93ba50179be85afd8cad8ea
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-04-27 00:56:05 -1000

    update

--- FXMLTestController.java
+++ FXMLTestController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.paint.Color;
 import devices.*;
 
 public class FXMLTestController {
@@ -74,7 +75,9 @@ public class FXMLTestController {
     @FXML protected void changePane3(ActionEvent event) {
         accordion.setExpandedPane(t4);
     }
-
+    @FXML protected void checkIfEmpty(Action event) {
+        
+    }
     public void initialize() {
         t4.expandedProperty().addListener((obs, wasExpanded, isNowExpanded) -> {
             if (isNowExpanded) {
